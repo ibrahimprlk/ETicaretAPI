1d9b1d6b2e7a7a284ffd6f275380ebca7dd645e0
İbrahim Parlak <ibrahimparlak3694@gmail.com>
2024-11-24 22:42:35 +0300

    ilk yükleme

---
+++ Core/ETicaretAPI.Application/Repositories/IRepository.cs
@@ -0,0 +1,13 @@
+﻿using ETicaretAPI.Domain.Entities.Common;
+using Microsoft.EntityFrameworkCore;
+
+namespace ETicaretAPI.Application.Repositories
+{
+    public interface IRepository
+    {
+        public interface IRepository<T> where T : BaseEntity
+        {
+            DbSet<T> Table { get; }
+        }
+    }
+}
